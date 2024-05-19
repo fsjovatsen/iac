29ebf544092d2bf40e3fefb5ceaa5fdcda53af37
Frode Sjovatsen <frode.sjovatsen@gmail.com>
2024-05-19 22:51:53 +0200

    We now support prometheus

---
+++ prometheus.tf
@@ -0,0 +1,7 @@
+resource "helm_release" "prometheus" {
+  chart = "prometheus"
+  name  = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  version = "25.21.0"
+  values = [file("observability/prometheus-values.yaml")]
+}
